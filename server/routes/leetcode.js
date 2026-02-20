186b0b9ee6d239116c20fe5dac2edd2dc1b3a9ca
Om <omsingh8400@gmail.com>
2026-02-21 00:41:35 +0530

    fixed the axios error

--- server/routes/leetcode.js
+++ server/routes/leetcode.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const axios = require('axios');
 
 // @desc    Get user leetcode stats
 router.get('/:username', async (req, res) => {
